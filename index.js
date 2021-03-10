25e275c246ac0add797351fd85b9c059c11a8013
Patrick Furtak <patrick@patrickfurtak.com>
2021-03-09 22:21:30 -0500

    Small refactor for DRYness and clarity

--- index.js
+++ index.js
@@ -16,16 +16,28 @@ const processScores = async () => {
 
 const evaluateScores = (games) => {
   for (let i = 0; i < games.length; i++) {
-    scoreTally(games[i]);
+    tallyScores(games[i]);
   }
 };
 
-const scoreTally = (scoreSequence) => {
-  let playerOneScore = 0;
-  let playerTwoScore = 0;
-  let playerOneWins = 0;
-  let playerTwoWins = 0;
+const tallyScores = (scoreSequence) => {
+  let [playerOneScore, playerTwoScore, playerOneWins, playerTwoWins] = Array(
+    4
+  ).fill(0);
   let deuce = false;
+
+  const resetScores = () => {
+    playerOneScore = 0;
+    playerTwoScore = 0;
+  };
+
+  const enterDeuce = () => {
+    deuce = true;
+  };
+
+  const leaveDeuce = () => {
+    deuce = false;
+  };
 
   for (let i = 0; i < scoreSequence.length; i++) {
     if (scoreSequence[i] === 'A') {
@@ -37,25 +49,23 @@ const scoreTally = (scoreSequence) => {
     }
 
     if (playerOneScore === 3 && playerTwoScore === 3) {
-      deuce = true;
+      enterDeuce();
     }
 
     if (playerOneScore >= 4 && playerOneScore - playerTwoScore >= 2) {
       playerOneWins++;
-      playerOneScore = 0;
-      playerTwoScore = 0;
-      deuce = false;
+      resetScores();
+      leaveDeuce();
     } else if (playerOneScore > 3 && playerOneScore - playerTwoScore < 2) {
-      deuce = true;
+      enterDeuce();
     }
 
     if (playerTwoScore >= 4 && playerTwoScore - playerOneScore >= 2) {
       playerTwoWins++;
-      playerOneScore = 0;
-      playerTwoScore = 0;
-      deuce = false;
+      resetScores();
+      leaveDeuce();
     } else if (playerTwoScore > 3 && playerTwoScore - playerOneScore < 2) {
-      deuce = true;
+      enterDeuce();
     }
   }
 
